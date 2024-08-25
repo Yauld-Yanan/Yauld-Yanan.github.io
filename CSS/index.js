3eb5911e56caca40755ad9fd8fc662ce6880311f
Yauld-Yanan <1325193824@qq.com>
2024-08-25 17:52:53 +0800

    Add files via upload

---
+++ CSS/index.js
@@ -0,0 +1,14 @@
+const btn2 = document.getElementById("btn2")
+const img = document.getElementById("img")
+const arr = ["main.car1.webp", "main.car2.png", 
+    "main.car3.webp", "main.car4.webp",
+     "main.car5.webp",]
+// const index = 0
+btn2.onclick = function () {
+    // alert("hahaha")
+    // img.style.backgroundImage = "url(../image/main.car3.webp)";
+    // img.style.backgroundColor = "black"
+    img.style.backgroundColor = "red";
+
+    // document.getElementById("elementId").style.backgroundColor = "red";
+}
